f92277487f50a99a520c8cd44aa7a452f9282ba8
JaimeFine <jaimefine6@gmail.com>
2025-10-18 20:42:57 +0800

    Finally passed

--- median_of_arrays/median_of_arrays.cpp
+++ median_of_arrays/median_of_arrays.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         while (nums1.size() > 2 && nums2.size() > 2) {
-            int med1, med2;
+            double med1, med2;
+
             med1 = (nums1.size() % 2 == 0)
-                ? (nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2 : nums1[nums1.size() / 2];
+                ? (static_cast<double>(nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1])) / 2 : nums1[nums1.size() / 2];
             med2 = (nums2.size() % 2 == 0) 
-                ? (nums2[nums2.size() / 2] + nums2[nums2.size() / 2 - 1]) / 2 : nums2[nums2.size() / 2];
+                ? (static_cast<double>(nums2[nums2.size() / 2] + nums2[nums2.size() / 2 - 1])) / 2 : nums2[nums2.size() / 2];
+
+            int m = std::min(nums1.size(), nums2.size());
+            m = (m % 2 == 0) ? m / 2 - 1 : m / 2; 
+
             if (med1 < med2) {
-                nums1.assign(nums1.begin() + med1, nums1.end());
-                nums2.assign(nums2.begin(), nums2.begin() + med1);
+                nums1.assign(nums1.begin() + m, nums1.end());
+                nums2.resize(nums2.size() - m);
             } else {
-                nums2.assign(nums2.begin() + med2, nums2.end());
-                nums1.assign(nums1.begin(), nums1.begin() + med2);
+                nums2.assign(nums2.begin() + m, nums2.end());
+                nums1.resize(nums1.size() - m);
             }
         }
 
         nums1.insert(nums1.end(), nums2.begin(), nums2.end());
         std::sort(nums1.begin(), nums1.end());
-        double med = (nums1.size() % 2 == 0)
+        double med;
+
+        med = (nums1.size() % 2 == 0)
             ? static_cast<double>(nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2
             : nums1[nums1.size() / 2];
 
