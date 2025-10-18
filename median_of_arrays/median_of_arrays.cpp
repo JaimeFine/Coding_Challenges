bba10e187fda512123e2542b978b9f912aa819a8
JaimeFine <jaimefine6@gmail.com>
2025-10-18 20:06:21 +0800

    First attempt after 1.25 hours

--- median_of_arrays/median_of_arrays.cpp
+++ median_of_arrays/median_of_arrays.cpp
@@ -1,48 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int med = 0;
         std::vector<double> merged;
         merged.reserve(nums1.size() + nums2.size());
 
-        if (num1.back() <= num2.front() || num2.back() <= num1.front()) {
-            merged.insert(merged.end(), num1.begin(), num1.end());
-            merged.insert(merged.end(), num2.begin(), num2.end());
-            int med, flag;
-            flag = (num1.size() + num2.size()) % 2;
-            if (flag == 1) {
-                med = (num1.size() + num2.size()) / 2;
-                return (merged[med] + merged[med + 1]) / 2;
+        while (nums1.size() > 2 && nums2.size() > 2) {
+            int med1, med2;
+            med1 = if (nums1.size() % 2 == 0) ? nums1[(nums1.size() - 1) / 2] : nums1[nums1.size() / 2];
+            med2 = if (nums2.size() % 2 == 0) ? nums2[(nums2.size() - 1) / 2] : nums2[nums2.size() / 2];
+            if (med1 < med1) {
+                nums1.assign(nums1.begin() + med1, nums1.end());
+                nums2.assign(nums2.begin(), nums2.begin() + med1);
             } else {
-                return (merged[med]);
+                nums2.assign(nums2.begin() + med2, nums2.end());
+                nums1.assign(nums1.begin(), nums1.begin() + med2);
             }
         }
 
-        for () {
-            if (num1.size() % 2 == 0) {
-                med1 = (num1[num1.size()/2] + num1[num1.size()/2 + 1]) / 2;
-            } else {
-                med1 = (num1[num1.size()/2 + 1]);
-            }
-            if (num2.size() % 2 == 0) {
-                med2 = (num2[num2.size()/2] + num2[num2.size()/2 + 1]) / 2;
-            } else {
-                med2 = (num2[num2.size()/2 + 1]);
-            }
+        merged = nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        std::sort(merged.begin(), merged.end());
+        med = if (merged.size() % 2 == 0) ? merged[(merged.size() - 1) / 2] : merged[merged.size() / 2];
 
-            if (med1 > med2) {
-                b2.insert(b2.end, num2.begin(), num2[med2];
-                num2 = b2;
-                b1.insert(b1.end, num1[med1], num1.end());
-                num1 = b1;
-            } else {
-                b1.insert(b1.end, num1.begin(), num1[med1];
-                num1 = b1;
-                b2.insert(b2.end, num2[med2], num2.end());
-                num2 = b2;
-            }
-        }
+        return med;
     }
 };
