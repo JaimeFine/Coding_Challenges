1dc360df96b5015eb82ce8da7c2db9d45b471792
JaimeFine <jaimefine6@gmail.com>
2025-10-18 20:19:40 +0800

    Third attempt

--- median_of_arrays/median_of_arrays.cpp
+++ median_of_arrays/median_of_arrays.cpp
@@ -5,12 +5,12 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int med = 0;
-
         while (nums1.size() > 2 && nums2.size() > 2) {
             int med1, med2;
-            med1 = (nums1.size() % 2 == 0) ? nums1[(nums1.size() - 1) / 2] : nums1[nums1.size() / 2];
-            med2 = (nums2.size() % 2 == 0) ? nums2[(nums2.size() - 1) / 2] : nums2[nums2.size() / 2];
+            med1 = (nums1.size() % 2 == 0)
+                ? (nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2 : nums1[nums1.size() / 2];
+            med2 = (nums2.size() % 2 == 0) 
+                ? (nums2[nums2.size() / 2] + nums2[nums2.size() / 2 - 1]) / 2 : nums2[nums2.size() / 2];
             if (med1 < med2) {
                 nums1.assign(nums1.begin() + med1, nums1.end());
                 nums2.assign(nums2.begin(), nums2.begin() + med1);
@@ -22,7 +22,9 @@ public:
 
         nums1.insert(nums1.end(), nums2.begin(), nums2.end());
         std::sort(nums1.begin(), nums1.end());
-        med = (nums1.size() % 2 == 0) ? nums1[(nums1.size() - 1) / 2] : nums1[nums1.size() / 2];
+        double med = (nums1.size() % 2 == 0)
+            ? static_cast<double>(nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2
+            : nums1[nums1.size() / 2];
 
         return med;
     }
