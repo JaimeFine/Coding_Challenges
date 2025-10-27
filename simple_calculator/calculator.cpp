7af65577956608a338e815af4378406d571592b6
JaimeFine <jaimefine6@gmail.com>
2025-10-27 13:53:15 +0800

    Saving temporarily

--- simple_calculator/calculator.cpp
+++ simple_calculator/calculator.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <cstdlib>
 #include <vector>
+#include <cmath>
+
+void basic_calculate(std::vector<int>& container);
+void bracket(std::vector<int>& container);
 
 void arg_parse(std::string input, std::vector<int>& container) {
     bool isNumber = false;
@@ -16,7 +20,8 @@ void arg_parse(std::string input, std::vector<int>& container) {
             current = current * 10 + (str[i] - '0');
             isNumber = true;
         } else if (
-            str[i] == '-' || str[i] == '+' || str[i] == '*' || str[i] == '/'
+            str[i] == '-' || str[i] == '+' || str[i] == '*' || str[i] == '/' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '^'
         ) {
             if (isNumber) {
                 container.push_back(current);
@@ -24,6 +29,8 @@ void arg_parse(std::string input, std::vector<int>& container) {
                 isNumber = false;
             }
             container.push_back(str[i]);
+        } else if (str[i] == ' ') {
+            continue;
         } else {
             std::cerr << "character '" << static_cast<char>(str[i]) 
             << "' not recognized!" << std::endl;
@@ -36,11 +43,35 @@ void arg_parse(std::string input, std::vector<int>& container) {
     }
 }
 
-void calculate(std::vector<int>& container) {
+void bracket(std::vector<int>& container) {
+    for ()
+}
+
+void basic_calculate(std::vector<int>& container) {
+    bracket(container);
+
+    for (int i = 0; i < container.size(); ++i) {
+        if (container[i] == '^') {
+            int left = container[i - 1];
+            int right = container[i + 1];
+            int result =static_cast<int>(pow(left, right));
+
+            container[i - 1] = result;
+            container.erase(container.begin() + i, container.begin() + i + 2);
+            i -= 1;
+        }
+    }
+
     for (int i = 0; i < container.size(); ++i) {
         if (container[i] == '*' || container[i] == '/') {
             int left = container[i - 1];
             int right = container[i + 1];
+
+            if (container[i] == '/' && right == 0) {
+                 std::cerr << "Error: Division by zero." << std::endl;
+                 exit(-1);
+            }
+
             int result = (container[i] == '*') ? left * right : left / right;
 
             container[i - 1] = result;
@@ -71,7 +102,7 @@ int main() {
     std::cin >> input;
 
     arg_parse(input, container);
-    calculate(container);
+    basic_calculate(container);
 
     std::cout << container[0] << std::endl;
 
